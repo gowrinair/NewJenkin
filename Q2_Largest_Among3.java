072175835f790dd272f5efa03d1b98239d834ce3
gowrinair <55692062+gowrinair@users.noreply.github.com>
2021-07-26 16:31:26 +0530

    Update Q2_Largest_Among3.java

--- Q2_Largest_Among3.java
+++ Q2_Largest_Among3.java
@@ -1,16 +1,12 @@
 package Week2_Programs;
 
-import java.util.Scanner;
+
 
 public class Q2_Largest_Among3 {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a:");
-		int a=sc.nextInt();
-		System.out.println("Enter b: ");
-		int b=sc.nextInt();
-		System.out.println("Enter c");
-		int c=sc.nextInt();
+		int a=10;
+		int b=16;
+		int c=2;
 		if(a>=b&&a>=c)
 			System.out.println("a is largest");
 		else if (b>c)
